2761a09eae877d13e55879ae7e3f710769f84a3e
Novokreschennih <Novokreschennih@gmail.com>
2025-09-28 11:46:58 +0800

    Update .eleventy.js

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -1,17 +1,16 @@
 const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation"); // <-- 1. Импортируем плагин
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-
-  // 2. Регистрируем плагин в Eleventy
+  // Регистрируем плагин навигации
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
   // Шорткод для автоматической подстановки текущего года
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
-  // Копируем статические файлы
+  // Копируем важные папки
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("static/css");
   eleventyConfig.addPassthroughCopy("static/img");
 
   // Фильтры
